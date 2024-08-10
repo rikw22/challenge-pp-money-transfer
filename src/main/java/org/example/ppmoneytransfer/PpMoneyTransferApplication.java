9ad70952addaadbef8da24fb9fc6e739ce1157b5
Rick <ricardoa.walter@gmail.com>
2024-08-10 20:54:13 -0300

    build: initial spring structure

---
+++ src/main/java/org/example/ppmoneytransfer/PpMoneyTransferApplication.java
@@ -0,0 +1,13 @@
+package org.example.ppmoneytransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpMoneyTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpMoneyTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ppmoneytransfer/PpMoneyTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ppmoneytransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpMoneyTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
