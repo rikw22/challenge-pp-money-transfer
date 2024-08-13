8a7c7cee8f7845f9928c1f7999abb17a0df15c25
Rick <ricardoa.walter@gmail.com>
2024-08-13 00:07:12 -0300

    chore: added logger to ExceptionHandler

--- src/main/java/org/example/ppmoneytransfer/commons/GlobalExceptionHandler.java
+++ src/main/java/org/example/ppmoneytransfer/commons/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.ppmoneytransfer.commons;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.ppmoneytransfer.exceptions.BusinessRuleException;
 import org.example.ppmoneytransfer.exceptions.ValidationError;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -20,11 +22,13 @@ public class GlobalExceptionHandler {
                 .map(error -> error.getField() + " " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
 
+        log.error("MethodArgumentNotValidException: {}", errorMessages, ex);
         return ResponseEntity.badRequest().body(new ValidationError(false, errorMessages));
     }
 
     @ExceptionHandler(BusinessRuleException.class)
     public ResponseEntity<ValidationError> handleBusinessRuleExceptions (BusinessRuleException ex) {
+        log.error("BusinessRuleException", ex);
         return ResponseEntity.badRequest().body(new ValidationError(false, ex.getMessage()));
     }
 
