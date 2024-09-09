f809a01621ff92407563072df1073e8c9e7c2bfd
Ricardo Walter <ricardoa.walter@gmail.com>
2024-09-09 01:01:20 -0300

    refactor: added validation annotations to Person entity to enforce data integrity

--- src/main/java/org/example/ppmoneytransfer/domain/Person.java
+++ src/main/java/org/example/ppmoneytransfer/domain/Person.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "people")
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,17 +15,23 @@ public class Person {
     Integer id;
 
     @NotNull
+    @Column(name = "full_name", nullable  = false)
     String fullName;
 
     @NotNull
+    @Column(name = "document", nullable  = false, unique = true)
     String document;
 
     @NotNull
+    @Column(name = "email", nullable  = false, unique = true)
     String email;
 
     @NotNull
+    @Column(name = "password", nullable  = false)
     String password;
 
     @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable  = false)
     PersonType type;
 }
